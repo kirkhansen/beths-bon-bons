d25f2ffa081c1dceb32623845d30ed9aef0e7e14
kirk.hansen <3529086+kirkhansen@users.noreply.github.com>
2025-01-13 15:45:17 -0600

    Update front page a bit more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,26 +12,26 @@ const Home: React.FC = () => {
   const images = importAll(require.context('../images/home_page_gallery', false, /\.(png|jpe?g|svg)$/));
 
   return (
-      <div>
-      <div className="d-flex justify-content-center align-items-center">
+    <div>
+      <div className="d-flex justify-content-center align-items-center text-center">
         <p>Crafting scrumptious cake pops and gourmet treats for central Iowa since 2022</p>
       </div>
       <main className="d-flex justify-content-center row">
-      <div className="col-lg-8">
-        <Carousel className="d-flex justify-content-center">
-          {images.map((src, index) => (
-          <CarouselItem key={index}>
-            <Image src={src} alt={`Slide ${index}`} className="img-fluid"/>
-          </CarouselItem>
-          ))}
-        </Carousel>
-      </div>
-      <div className="d-flex justify-content-center align-items-center row">
-        <div className="col-lg-8 justify-content-center">
-            <Image className="img-fluid" src={siteLogo} alt="Beth's Bon Bons & Cake Pops"></Image>
+        <div className="col-lg-8">
+          <Carousel className="d-flex justify-content-center">
+            {images.map((src, index) => (
+            <CarouselItem key={index}>
+              <Image src={src} alt={`Slide ${index}`} className="img-fluid"/>
+            </CarouselItem>
+            ))}
+          </Carousel>
         </div>
-      </div>
-    </main>
+        <div className="d-flex justify-content-center align-items-center row">
+          <div className="col-lg-6 justify-content-center">
+              <Image className="img-fluid" src={siteLogo} alt="Beth's Bon Bons & Cake Pops"></Image>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
