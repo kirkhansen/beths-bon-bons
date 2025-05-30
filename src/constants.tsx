2eacdebe57a93f46a10bc0d38331f40f4270470d
kirk.hansen <3529086+kirkhansen@users.noreply.github.com>
2025-05-30 00:53:37 -0500

    update order form

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-export const ORDER_FORM_URI = "https://script.google.com/macros/s/AKfycbxRZSAsUw_AUzXbCbf3qdK5QOkQduqlqpUim5rVhTgKD1THoYFxE5RgkWKIyhiYx_GhwA/exec"
+export const ORDER_FORM_URI = "https://script.google.com/macros/s/AKfycbxwBf-0jh8eItauRhCbhrTmBAyMiLisGdDUyexSiE9ManisZ_A6mdaVXeqpJfjiFB5NlA/exec"
 export const MAIL_CHIMP_URI = "https://app.us8.list-manage.com/subscribe/post?u=c72545f330723e5656eae34ce&id=5dfc19aaa6&f_id=000dc9e2f0";
 
 type PaymentMethod = "venmo" | "cash" | null;
